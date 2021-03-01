de6ac37d182ec9540635f3901c6c0dde5a123133
ThomasApplewhite <44822750+ThomasApplewhite@users.noreply.github.com>
2021-03-01 10:01:15 -0800

    Basic Voxeling
    It's a Mesh. Of Cubes! Isn't initialized in a super clean way, and there are a bunch of redundent vertices, but the basics work!

---
+++ Cave Generation/Assets/Scripts/VoxelData.cs
@@ -0,0 +1,71 @@
+﻿//using System.Linq;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This script is based off of one made by Board to Bits
+
+public class VoxelData
+{
+
+    private int[,,] dataArray;
+
+    private System.Random RNG;
+
+    public VoxelData(int size, Vector3 originPosition)
+    {
+        dataArray = new int[size, size, size];
+
+        RNG = new System.Random();
+
+        var initializer = new Func<Vector3, int>((Vector3 vec) => randomVal(vec));
+        
+        Initialize(dataArray, originPosition, initializer);
+    }
+
+    public int dataWidth
+    {
+        get {return dataArray.GetLength(0);}
+    }
+
+    public int dataHeight{
+        get {return dataArray.GetLength(1);}
+    }
+
+    public int dataDepth
+    {
+        get {return dataArray.GetLength(2);}
+    }
+
+    public int GetCell(int x, int y, int z)
+    {
+        return dataArray[x, y, z];
+    }
+
+    void SetCell(int val, int x, int y, int z)
+    {
+        dataArray[x, y, z] = val;
+    }
+
+    void Initialize(int[,,] array, Vector3 origin, Func<Vector3, int> initializer)
+    {
+        Vector3 position;
+
+        for(int x = 0; x < dataWidth; ++x)
+        {
+            for(int y = 0; y < dataHeight; ++y)
+            {
+                for(int z = 0; z < dataDepth; ++z)
+                {
+                    array[x, y, z] = initializer.Invoke(new Vector3(x, y, z));
+                }
+            }
+        }
+    }
+
+    int randomVal(Vector3 x)
+    {
+        return RNG.Next() % 2;
+    }
+}
---
+++ Cave Generation/Assets/Scripts/VoxelRenderer.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+//This script is based off of one made by Board to Bits
+
+[RequireComponent (typeof(MeshFilter), typeof(MeshRenderer))]
+public class VoxelRenderer : MonoBehaviour {
+
+	Mesh mesh;
+	List<Vector3> vertices;
+	List<int> triangles;
+
+	public float scale = 1f;
+
+	public int size = 10;
+	public Vector3 origin = new Vector3(0, 0, 0);
+	//public int posX, posY, posZ;
+
+	float adjScale;
+
+
+
+	// Use this for initialization
+	void Awake () {
+		mesh = GetComponent<MeshFilter> ().mesh;
+		adjScale = scale * 0.5f;
+	}
+
+	void Start() {
+		//MakeCube (adjScale, new Vector3((float)posX * scale, (float)posY * scale, (float)posZ * scale));
+
+        GenerateVoxelMesh(new VoxelData(size, origin));
+		UpdateMesh ();
+	}
+
+    void GenerateVoxelMesh(VoxelData data)
+    {
+        vertices = new List<Vector3> ();
+		triangles = new List<int> ();
+
+        for(int z = 0; z < data.dataDepth; ++z)
+        {
+            for(int y = 0; y < data.dataHeight; ++y)
+			{
+				for(int x = 0; x < data.dataWidth; ++x)
+            	{
+                	if(data.GetCell(x, y, z) == 0)
+                	{
+                	    continue;
+                	}
+                	else
+                	{	//
+                	    MakeCube(adjScale, new Vector3((float)x * scale, (float)y * scale, (float)z * scale));
+                	}
+            	}
+			}
+        }
+    }
+
+	void MakeCube (float cubeScale, Vector3 cubePos){
+		for (int i = 0; i < 6; i++) {
+			MakeFace (i, cubeScale, cubePos);
+		}
+	}
+
+	void MakeFace (int dir, float faceScale, Vector3 facePos){
+		vertices.AddRange (CubeMeshData.faceVertices (dir, faceScale, facePos));
+		int vCount = vertices.Count;
+
+		triangles.Add (vCount - 4);
+		triangles.Add (vCount - 4 + 1);
+		triangles.Add (vCount - 4 + 2);
+		triangles.Add (vCount - 4);
+		triangles.Add (vCount - 4 + 2);
+		triangles.Add (vCount - 4 + 3);
+
+	}
+
+	void UpdateMesh(){
+		mesh.Clear ();
+
+		mesh.vertices = vertices.ToArray();
+		mesh.triangles = triangles.ToArray();
+		mesh.RecalculateNormals ();
+	}
+}
